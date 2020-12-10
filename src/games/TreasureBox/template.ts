25767da5d5c6fd0d805ba2b000bae42fe0131e9a
eightfeet <bajiao000000@163.com>
2020-12-10 11:57:14 +0800

    更新模板

--- src/games/TreasureBox/template.ts
+++ src/games/TreasureBox/template.ts
@@ -2,19 +2,49 @@ import { html } from 'common-tags';
 import { Prize } from '~/types/core';
 import s from './index.scss';
 
+export const renderMax4 = (prizes) => {
+    return html`
+        <div class="${s.prizes}">
+            ${prizes.map(
+                (item: Prize) =>
+                    html`<div class="${s.item}">
+                        <div class="${s.imgwrap}">
+                            <img src="${item.gameImg}" />
+                        </div>
+                        <p style="width:${30 / prizes.length}em">
+                            ${item.prizeAlias}
+                        </p>
+                    </div>`
+            )}
+        </div>
+    `;
+};
+
+export const renderMin5 = (prizes) => {
+    return html`
+        <div class="${s.prizemin5}">
+            <div class="${s.prizesliderbox}">
+                ${prizes.map(
+                    (item: Prize) =>
+                        html`<div class="${s.item}">
+                            <div class="${s.imgwrap}">
+                                <img src="${item.gameImg}" />
+                            </div>
+                            <p style="width:${7.5}em">
+                                ${item.prizeAlias}
+                            </p>
+                        </div>`
+                )}
+            </div>
+        </div>
+    `;
+};
+
 export const renderGame = ({ prizes }) => {
     return html`
         <div class="${s.root}">
             <div class="${s.prizebox}">
-                <div class="${s.prizes}">
-                    ${prizes.map(
-                        (item: Prize) =>
-                            `<div class="${s.item}">
-                            <img src="${item.gameImg}" />
-                            ${item.prizeAlias}
-                            </div>`
-                    )}
-                </div>
+                ${prizes.length <= 4 ? renderMax4(prizes) : renderMin5(prizes)}
             </div>
             <div class="${s.btnwrap}"><div class="${s.startbtn}"></div></div>
             <div class="${s.box}"></div>
