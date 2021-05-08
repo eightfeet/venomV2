198184e46f36f61b781ac99263209b5a1c335f64
eightfeet <bajiao000000@163.com>
2021-05-08 10:49:58 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import data from '../config';
 let dom = `<h1>CATEGORY</h1><ul>`;
 
 for (let index = 1; index < data.length; index++) {
-    const element = data[index];
-    dom += `<li><h3><a href="./${element.name}.html">${element.templatename}</a></h3></li>`;
+	const element = data[index];
+	dom += `<li><h3><a href="./${element.name}.html">${element.templatename}</a></h3></li>`;
 }
 
 dom += '</ul>';
